3d53dee59b7b321ee5de70705b59d576f3711bc9
Arvid <arvidaxelsson1995@gmail.com>
2019-11-22 13:02:28 +0100

    Made the Exp table.

---
+++ MyCV/ExperienceDetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ExperienceDetailViewController.swift
+//  MyCV
+//
+//  Created by Arvid Axelsson on 2019-11-22.
+//  Copyright © 2019 Arvid Axelsson. All rights reserved.
+//
+
+import UIKit
+
+class ExperienceDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
